b438e11dee26fdabf4c9d95c2630244d7d11532c
Pop Cristian-Gabriel <57450421+PopCristianGabriel@users.noreply.github.com>
2021-03-29 21:11:37 +0300

    added more tests

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -1,9 +1,14 @@
 package ssvv.example;
 
+import org.junit.Before;
 import org.junit.Test;
 import ssvv.example.domain.Student;
+import ssvv.example.domain.Tema;
 import ssvv.example.repository.StudentRepository;
+import ssvv.example.repository.TemaRepository;
 import ssvv.example.validation.StudentValidator;
+import ssvv.example.validation.TemaValidator;
+import ssvv.example.validation.ValidationException;
 import ssvv.example.validation.Validator;
 
 import java.util.Collection;
@@ -13,38 +18,59 @@ import static org.junit.Assert.*;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
+    StudentRepository studentRepository;
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+
+    @Before
+    public void setup(){
+        Validator<Student> studentValidator = new StudentValidator();
+        this.studentRepository = new StudentRepository(studentValidator);
     }
 
     @Test
-    public void tc_1_addStudentWithValidGroup(){
-        // given
-        Validator<Student> studentValidator = new StudentValidator();
-        StudentRepository studentRepository = new StudentRepository(studentValidator);
-        // when
-        Student s = studentRepository.save(new Student("17", "cristi", 600));
-        // then
+    public void tc_1_addStudentWithValidGroup() {
+        Student s = this.studentRepository.save(new Student("17", "cristi", 600));
         assertNull(s);
-        studentRepository.delete("77");
     }
+
     @Test
-    public void addStudentWithInvalidGroup(){
-        // given
-        Validator<Student> studentValidator = new StudentValidator();
-        StudentRepository studentRepository = new StudentRepository(studentValidator);
-        // when
+    public void addStudentWithInvalidGroup() {
         Student s = studentRepository.save(new Student("17", "cristi", -1));
-        // then
         assertNull(s);
-        studentRepository.delete("77");
     }
+
+    @Test
+    public void AddStudentInvalidName() {
+        Student s = studentRepository.save(new Student("17", "", 600));
+        assertNull(s);
+    }
+
+    @Test
+    public void addStudentValidName(){
+        Student s = studentRepository.save(new Student("17", "dawd", 600));
+        assertNull(s);
+    }
+
+    @Test
+    public void addStudentValidId(){
+        Student s = studentRepository.save(new Student("17", "dawd", 600));
+        assertNull(s);
+    }
+
+    @Test
+    public void addStudentInvalidId(){
+        Student s = studentRepository.save(new Student("", "dawd", 600));
+        assertNull(s);
+    }
+
 
 }
